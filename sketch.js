7a6431f3f25b04a1414a021934cd9ad59b8de2d5
Mike Kuijper <36420387+MikeKuijper@users.noreply.github.com>
2019-03-17 21:03:06 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -59,6 +59,17 @@ function reset() {
 
 let canvas;
 
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
 function setup() {
   let parent = document.getElementById("sketch-holder");
   canvas = createCanvas(parent.offsetWidth, 500);
